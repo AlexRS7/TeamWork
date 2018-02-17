634724a5633baff7c17663db73d405b8a028ede0
Alexandru Vasile <alexandru.vasile7@yahoo.com>
2018-02-17 12:08:09 +0200

    Am facut o modificare ,gaseste diferanta :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] anArray = new int[n];
+        for (int i = 0; i < n; ++i) {
+            anArray[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        boolean z = Search(n, anArray,x);
+        Search(n, anArray,y);
+    }
+
+    public static boolean Search(int n, int[] anArray, int x) {
+        for (int s : anArray) {
+            if (s == x)
+                return true;
         
     }
 }
